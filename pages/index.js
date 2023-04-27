533bdbb6ee21fc24a387b79dfb266d1d29b4cfee
JRH89 <thegemlins@gmail.com>
2023-04-26 19:55:36 -0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,800;0,900;1,200&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        <link rel="shortcut icon" type="image/png" href="public/Hooker Hill.png" />
+        <link rel="shortcut icon" type="image/png" href="public/HookerHill.png" />
         <title>ToDO</title>
       </Head>
       {!currentUser && <Login />}

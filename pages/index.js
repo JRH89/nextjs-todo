fe9fd2d9cdd6dbc5a95f5eb09618a4d389441975
JRH89 <thegemlins@gmail.com>
2023-04-26 20:01:53 -0700

    actually fixed meta tags this time

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,16 @@ export default function Home() {
         <meta property="og:url" content="https://nextjs-blog-jrh89.vercel.app" />
         <meta property="og:title" content="ToDo" />
         <meta property="og:description" content="A simple ToDo app made with NextJS, TailwindCSS, and Firebase" />
-        <meta property="og:image" content="public/cover-todo.png" />
+        <meta property="og:image" content="cover-todo.png" />
+        <meta property="url" content="https://nextjs-blog-jrh89.vercel.app" />
+        <meta property="title" content="ToDo" />
+        <meta property="description" content="A simple ToDo app made with NextJS, TailwindCSS, and Firebase" />
+        <meta property="image" content="cover-todo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,800;0,900;1,200&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-        <link rel="shortcut icon" type="image/png" href="public/HookerHill.png" />
+        <link rel="shortcut icon" type="image/png" href="HookerHill.png" />
         <title>ToDO</title>
       </Head>
       {!currentUser && <Login />}

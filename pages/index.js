eebdab61d0bc0164dc77daf4b654ba92a0c31f4c
JRH89 <thegemlins@gmail.com>
2023-04-27 20:05:23 -0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,11 @@ export default function Home() {
         <meta property="og:url" content="https://nextjs-todo-rose.vercel.app" />
         <meta property="og:title" content="ToDo" />
         <meta property="og:description" content="A simple ToDo app made with NextJS, TailwindCSS, and Firebase" />
-        <meta property="og:image" content="/cover-todo.png" />
+        <meta property="og:image" content="cover-todo.png" />
         <meta property="url" content="https://nextjs-todo-rose.vercel.app" />
         <meta property="title" content="ToDo" />
         <meta property="description" content="A simple ToDo app made with NextJS, TailwindCSS, and Firebase" />
-        <meta property="image" content="/cover-todo.png" />
+        <meta property="image" content="cover-todo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,500;0,800;0,900;1,200&display=swap" rel="stylesheet" />
